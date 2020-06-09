520a93694f839af8f7defed4e5e72ab2ad4dbd74
Karl <karl@karlblum.net>
2020-06-09 17:18:25 +0300

    updated conf

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {};
 
-config.telnet_host = '192.168.1.100';
+config.telnet_host = '192.168.1.168';
 config.telnet_port = 23;
 config.telnet_timeout = 3000;
 config.telnet_requestTimeout = 300;
